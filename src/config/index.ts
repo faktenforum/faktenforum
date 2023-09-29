8b0cce5d62267ca33d512ddd017236ef1fcad95c
bw <benjamin.werner42@pm.com>
2023-09-29 15:25:52 +0200

    improve enviroment variable handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,13 +11,3 @@ export const config: Partial<TsED.Configuration> = {
   logger: loggerConfig
   // additional shared configuration
 };
-
-export function ensureEnvironmentVariables() {
-  const requiredEnvVars = ["JWT_SECRET", "JWT_REFRESH_TOKEN_LIFETIME", "JWT_TOKEN_TOKEN_LIFETIME"];
-
-  for (const varName of requiredEnvVars) {
-    if (!process.env[varName]) {
-      throw new Error(`Environment variable ${varName} is not set.`);
-    }
-  }
-}
