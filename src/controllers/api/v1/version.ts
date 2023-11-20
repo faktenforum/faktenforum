de213e6e3b387dd2a7338fc5636267f5647834a8
bw <benjamin.werner42@pm.com>
2023-11-20 16:28:29 +0100

    fix version tag

--- src/controllers/api/v1/version.ts
+++ src/controllers/api/v1/version.ts
@@ -1,13 +1,13 @@
 import { Controller } from "@tsed/di";
 import { Get } from "@tsed/schema";
-import { version } from "../../../../package.json"; // adjust the path as necessary
+const packageJSON = require("../../../../package.json"); // adjust the path as necessary
 
 @Controller("/version")
 export class VersionController {
   @Get()
   async getVersion() {
     return {
-      version
+      version: packageJSON.version
     };
   }
 }
