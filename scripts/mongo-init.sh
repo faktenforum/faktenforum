d102103d2a18ce8fc630b23b511500de28f6da18
Benjamin Werner <bewr@pm.me>
2023-11-20 09:25:48 +0100

    working on deployment

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -1,11 +1,11 @@
 set -e
 mongosh -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin <<EOF
 use ${MONGODB_AGENDA_DB}
-
 db.createUser({
   user: '${MONGODB_AGENDA_USER}',
   pwd: '${MONGODB_AGENDA_PASSWORD}',
   roles: [{ role: 'readWrite', db: '${MONGODB_AGENDA_DB}' },
   { role: 'dbOwner', db: '${MONGODB_AGENDA_DB}' }],
 });
+
 EOF
