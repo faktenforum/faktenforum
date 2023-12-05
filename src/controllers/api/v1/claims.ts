a08c11d357859a8899d1d64c59e9d9909537ab7a
Benjamin Werner <bewr@pm.me>
2023-12-05 09:11:56 +0100

    WIP

--- src/controllers/api/v1/claims.ts
+++ src/controllers/api/v1/claims.ts
@@ -3,7 +3,7 @@ import { PathParams } from "@tsed/platform-params";
 import { QueryParams } from "@tsed/platform-params";
 import { Get, Returns } from "@tsed/schema";
 import { AccessControlDecorator } from "~/decorators";
-import { ClaimQueryParams } from "~/models";
+import { ClaimQueryParams, ClaimWithResources, Pagination } from "~/models";
 import { ClaimService } from "~/services";
 
 @Controller("/claims")
@@ -13,13 +13,14 @@ export class ClaimsController {
 
   // @AccessControlDecorator({ role: "ADMIN" })
   @Get()
-  @Returns(200, SubmissionResponse)
+  @Returns(200, Pagination).Of(ClaimWithResources)
   async getClaims(@QueryParams() params: ClaimQueryParams) {
     const claims = await this.claimService.getClaims(params);
     return claims;
   }
   @AccessControlDecorator({ role: "ADMIN" })
   @Get("/:id")
+  @Returns(200, ClaimWithResources)
   async getClaim(@PathParams("id") id: string) {
     const claim = await this.claimService.getClaimById(id);
     return claim;
