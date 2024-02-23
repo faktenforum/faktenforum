8e590344247a27bacea252ca3835a9730a098fd9
Benjamin Werner <bewr@pm.me>
2024-02-23 16:35:43 +0100

    disable vitest globals explicit wasn't working anyways

--- vitest.config.js
+++ vitest.config.js
@@ -1,39 +1,39 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { viteSingleFile } from 'vite-plugin-singlefile';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [viteSingleFile(), tsconfigPaths()],
   test: {
-    globals: true,
-    environment: 'node',
+    globals: false,
+    environment: "node",
     // Collect coverage
     coverage: {
       enabled: true,
       // Add equivalent settings to your Jest coverage configuration
-      reporter: ['text', 'json', 'html'],
-      exclude: ['index.ts', '**/node_modules/**','dist'],
+      reporter: ["text", "json", "html"],
+      exclude: ["index.ts", "**/node_modules/**", "dist"],
       // Set your threshold
       threshold: {
         statements: 0,
         branches: 0,
         functions: 0,
-        lines: 0,
-      },
+        lines: 0
+      }
     },
     // Test files pattern
-    include: ['**/src/**/__tests__/**/*.ts', '**/src/**/?(*.)+(spec|test).ts'],
+    include: ["**/src/**/__tests__/**/*.ts", "**/src/**/?(*.)+(spec|test).ts"],
     // Transform settings
     transformMode: {
-      web: [/\.[jt]sx?$/],
+      web: [/\.[jt]sx?$/]
     },
     // Module name mapper
     resolve: {
       alias: {
-        '^~/(.*)$': resolve(__dirname, './src/$1'),
+        "^~/(.*)$": resolve(__dirname, "./src/$1")
         // Add other path mappings if needed
-      },
-    },
-  },
+      }
+    }
+  }
 });
