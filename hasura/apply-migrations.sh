f5f5d83a3f963dcc131fa4832ac02ff41e880fce
bw <benjamin.werner42@pm.com>
2024-03-06 18:00:06 +0100

    add github action to build hasura migrate

--- hasura/apply-migrations.sh
+++ hasura/apply-migrations.sh
@@ -11,27 +11,45 @@
 : ${APPLY_MIGRATIONS:=false}
 : ${APPLY_METADATA:=false}
 : ${APPLY_SEEDS:=false}
+: ${WAIT_FOR_ENDPOINT:=true}
+
+# Wait for the endpoint to be available
+if [ "$WAIT_FOR_ENDPOINT" = "true" ]; then
+    echo "Waiting for Hasura endpoint $HASURA_ENDPOINT to be available..."
+    while true; do
+        if curl -s -o /dev/null "$HASURA_ENDPOINT/v1/version"; then
+            echo "Endpoint $HASURA_ENDPOINT is now available."
+            break
+        else
+            echo "Endpoint $HASURA_ENDPOINT not available, waiting..."
+            sleep 1
+        fi
+    done
+fi
+
+# Apply seeds if enabled
+
 
 # Apply migrations if enabled
 if [ "$APPLY_MIGRATIONS" = "true" ]; then
     echo "Applying migrations..."
-    hasura migrate apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET --skip-update-check
+    hasura migrate apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET 
 else
     echo "Skipping migrations."
+fi
+
+if [ "$APPLY_SEEDS" = "true" ]; then
+    echo "Applying seeds..."
+    hasura seed apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET 
+else
+    echo "Skipping seeds."
 fi
 
 # Apply metadata if enabled
 if [ "$APPLY_METADATA" = "true" ]; then
     echo "Applying metadata..."
-    hasura metadata apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET --skip-update-check
+    hasura metadata apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET 
 else
     echo "Skipping metadata."
 fi
 
-# Apply seeds if enabled
-if [ "$APPLY_SEEDS" = "true" ]; then
-    echo "Applying seeds..."
-    hasura seed apply --endpoint $HASURA_ENDPOINT --admin-secret $HASURA_ADMIN_SECRET --skip-update-check
-else
-    echo "Skipping seeds."
-fi
