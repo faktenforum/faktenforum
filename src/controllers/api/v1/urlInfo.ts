4fedb883ca2bd1acd715b5f29186cd517e16c6f2
Tobsic <tobias.brosge@port-zero.com>
2024-07-30 20:37:24 +0200

    url-info: Handle invalid urls

--- src/controllers/api/v1/urlInfo.ts
+++ src/controllers/api/v1/urlInfo.ts
@@ -1,21 +1,43 @@
 import { Controller } from "@tsed/di";
-import { BadRequest } from "@tsed/exceptions";
+import { BadRequest, NotFound } from "@tsed/exceptions";
 import { PathParams } from "@tsed/platform-params";
-import { Get, Returns } from "@tsed/schema";
+import { ContentType, Get, Returns } from "@tsed/schema";
 import getMetaData from "metadata-scraper";
 import { UrlInfoResponse } from "~/models/responses/UrlInfoResponse";
 
 @Controller("/url-info")
 export class UrlInfoController {
   @Get("/:url")
-  @Returns(200, UrlInfoResponse).ContentType("application/json")
+  @Returns(400)
+  @Returns(404)
+  @Returns(200, UrlInfoResponse)
+  @ContentType("application/json")
   async getUrlInfo(@PathParams("url") url: string) {
     const schemaMatch = url.match(/^(.*):\/\//);
     if (!schemaMatch) {
       url = `http://${url}`;
     } else if (!["http://", "https://"].includes(schemaMatch[0])) {
-      throw new BadRequest("URL contains unsupported schema");
+      throw new BadRequest("Invalid URL: Unsupported schema schema");
     }
-    return getMetaData(url);
+    try {
+      return await getMetaData(url);
+    } catch (e) {
+      switch (e.code) {
+        case "ENOTFOUND":
+          return new BadRequest("Invalid URL: Unable to resolve");
+
+        case "ERR_INVALID_URL":
+          return new BadRequest("Invalid URL: URL is malformed");
+
+        case "ERR_NON_2XX_3XX_RESPONSE":
+          if (e.response.statusCode === 404) {
+            return new NotFound("Invalid URL: Resource not found");
+          }
+          throw e;
+
+        default:
+          throw e;
+      }
+    }
   }
 }
