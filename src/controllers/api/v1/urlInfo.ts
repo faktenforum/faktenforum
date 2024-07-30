8873039ac80c11ee69ca6102cebfea2168a42a41
Tobsic <tobias.brosge@port-zero.com>
2024-07-30 19:59:05 +0200

    url-info: Use `http://` as default url schema, fail on urls with invalid schema

--- src/controllers/api/v1/urlInfo.ts
+++ src/controllers/api/v1/urlInfo.ts
@@ -1,4 +1,5 @@
 import { Controller } from "@tsed/di";
+import { BadRequest } from "@tsed/exceptions";
 import { PathParams } from "@tsed/platform-params";
 import { Get, Returns } from "@tsed/schema";
 import getMetaData from "metadata-scraper";
@@ -9,6 +10,12 @@ export class UrlInfoController {
   @Get("/:url")
   @Returns(200, UrlInfoResponse).ContentType("application/json")
   async getUrlInfo(@PathParams("url") url: string) {
+    const schemaMatch = url.match(/^(.*):\/\//);
+    if (!schemaMatch) {
+      url = `http://${url}`;
+    } else if (!["http://", "https://"].includes(schemaMatch[0])) {
+      throw new BadRequest("URL contains unsupported schema");
+    }
     return getMetaData(url);
   }
 }
