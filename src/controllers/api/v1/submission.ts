ca565ce93bf305dc9c874aabcd35507c339ecc87
Benjamin Werner <bewr@pm.me>
2024-09-24 17:01:43 +0200

    fix submission controller crash on no file uploads

--- src/controllers/api/v1/submission.ts
+++ src/controllers/api/v1/submission.ts
@@ -58,7 +58,7 @@ export class SubmissionController {
               }
             }
           : undefined;
-      if (files[fileIndex].mimetype.startsWith("image/")) {
+      if (files && files[fileIndex] && files[fileIndex].mimetype.startsWith("image/")) {
         // Resize and upload the image
         console.log("--------------------------------------resizing and uploading image");
         this.imageService.resizeAndUpload(files[fileIndex].key);
