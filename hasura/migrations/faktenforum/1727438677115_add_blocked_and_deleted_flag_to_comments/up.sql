df38838d07f5234fea343b362ade3099c4a0c5db
Benjamin Werner <bewr@pm.me>
2024-09-29 16:37:58 +0200

    Add delted and blocked to comment Add generate column displayed_content to comment

--- hasura/migrations/faktenforum/1727438677115_add_blocked_and_deleted_flag_to_comments/up.sql
+++ hasura/migrations/faktenforum/1727438677115_add_blocked_and_deleted_flag_to_comments/up.sql
@@ -2,4 +2,14 @@ ALTER TABLE public.comment
 ADD COLUMN deleted boolean DEFAULT FALSE;
 -- Add the censored column to the public.comment table
 ALTER TABLE public.comment
-ADD COLUMN censored boolean DEFAULT FALSE;
+ADD COLUMN blocked boolean DEFAULT FALSE;
+
+-- Add a generated column for displayed_content
+ALTER TABLE public.comment
+ADD COLUMN displayed_content TEXT GENERATED ALWAYS AS (
+    CASE
+        WHEN deleted THEN 'Content deleted'
+        WHEN blocked THEN 'Content blocked'
+        ELSE content
+    END
+) STORED;
