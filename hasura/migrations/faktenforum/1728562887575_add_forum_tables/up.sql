8b1f6b017a2dc34028f3b6624c448ae1e739821b
Benjamin Werner <bewr@pm.me>
2024-10-10 18:22:19 +0200

    fix channel table migration and seed

--- hasura/migrations/faktenforum/1728562887575_add_forum_tables/up.sql
+++ hasura/migrations/faktenforum/1728562887575_add_forum_tables/up.sql
@@ -1,17 +1,16 @@
 CREATE TABLE public.channel (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
-    minimum_acces_level integer NOT NULL,
+    name text NOT NULL,
     archived boolean NOT NULL,
-    private boolean NOT NULL,
-    image uuid,
+    internal boolean NOT NULL,
+    file_id uuid,  -- Added missing comma here
     label_de text NOT NULL,
     label_en text NOT NULL,
     description_de text NOT NULL,
-    description_en text NOT NULL,
+    description_en text NOT NULL
 );
 COMMENT ON TABLE public.channel IS 'Role Based Access Channels ';
 
-ALTER TABLE ONLY ALTER ALTER TABLE ONLY public.channel
-    ADD CONSTRAINT channel_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.channel  -- Removed redundant ALTER keyword
+    ADD CONSTRAINT channel_pkey PRIMARY KEY (name);
 ALTER TABLE ONLY public.channel
-    ADD CONSTRAINT channel_image_fkey FOREIGN KEY (image) REFERENCES public.file(id) ON UPDATE CASCADE ON DELETE SET NULL;
+    ADD CONSTRAINT channel_image_fkey FOREIGN KEY (file_id) REFERENCES public.file(id) ON UPDATE CASCADE ON DELETE SET NULL;
