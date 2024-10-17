0df9713d32d2e56354408d9ae221e96466b1ba28
Benjamin Werner <bewr@pm.me>
2024-10-17 21:28:38 +0200

    update metadta

--- hasura/migrations/faktenforum/1727114136960_add_user_profile_update_event/up.sql
+++ hasura/migrations/faktenforum/1727114136960_add_user_profile_update_event/up.sql
@@ -89,7 +89,6 @@ BEGIN
             v_user_id := NEW.updated_by;
             v_entry_id := NEW.id;
         ELSIF TG_TABLE_NAME = 'user' THEN
-            v_claim_id := null;
             v_user_id := OLD.id;
             v_entry_id := OLD.id;
         ELSE
