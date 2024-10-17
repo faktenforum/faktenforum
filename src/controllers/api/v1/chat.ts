826dcb8c3a6b510a6c8c7338469e933b4d7c48cc
Benjamin Werner <bewr@pm.me>
2024-10-17 12:45:33 +0200

    Working jwt login

---
+++ src/controllers/api/v1/chat.ts
@@ -0,0 +1,44 @@
+// src/controllers/AuthController.ts
+
+import { Controller, Get, Res, Inject, $log, Req } from "@tsed/common";
+import { Cookies } from "@tsed/platform-params";
+import { AuthService, EnvService } from "~/services";
+import jwt from "jsonwebtoken"; // Import the jsonwebtoken library
+import { AccessControlDecorator } from "~/decorators";
+import { Returns } from "@tsed/schema";
+import { Session } from "~/models";
+
+@Controller("/chat")
+export class AuthController {
+  @Inject(AuthService)
+  authService: AuthService;
+
+  @Inject(EnvService)
+  envService: EnvService;
+
+  @AccessControlDecorator({})
+  @(Returns(200, String).ContentType("*/*").Description("Generated JWT token") // prettier-ignore
+    ) // prettier-ignore
+  @(Returns(400, String).Description("Bad request. The request or parameters are incorrect.") // prettier-ignore
+    ) // prettier-ignore
+  @(Returns(401, String).Description("Unauthorized. Authentication credentials are missing or invalid.") // prettier-ignore
+    ) // prettier-ignore
+  @(Returns(404, String).Description("File not found. The requested file does not exist.") // prettier-ignore
+    ) // prettier-ignore
+  @(Returns(500, String).Description("Internal server error. An unexpected error occurred.") ) // prettier-ignore
+  @Get("/token")
+  async genToken(
+    @Cookies("ory_kratos_session") cookieSession: string,
+    @Req() request: Req,
+    @Res() response: Res
+  ) {
+    // Generate a JWT with the username (subject) using authlib
+
+    const payload = { sub: (request.user as Session).username };
+    const secret = "my-secret-token";
+    const token = jwt.sign(payload, secret, { algorithm: "HS256" });
+
+    // Send the JWT in the response
+    response.json({ token });
+  }
+}
--- src/models/Session.ts
+++ src/models/Session.ts
@@ -2,15 +2,40 @@ import { Property, Required } from "@tsed/schema";
 
 // Create an Enum of roles
 export enum UserRole {
-  ADMIN = "admin",
-  USER = "user",
-  NEWBIE = "newbie"
+  ADMINISTRATOR = "administrator",
+  EDITOR = "editor",
+  MODERATOR = "moderator",
+  SENIOR = "senior",
+  JUNIOR = "junior",
+  ASPIRANT = "aspirant"
 }
+
+export enum PowerLevel {
+  ADMINISTRATOR = 100,
+  EDITOR = 80,
+  MODERATOR = 60,
+  SENIOR = 40,
+  JUNIOR = 20,
+  ASPIRANT = 0
+}
+
+export const POWER_LEVELS = {
+  [UserRole.ADMINISTRATOR]: PowerLevel.ADMINISTRATOR,
+  [UserRole.EDITOR]: PowerLevel.EDITOR,
+  [UserRole.MODERATOR]: PowerLevel.MODERATOR,
+  [UserRole.SENIOR]: PowerLevel.SENIOR,
+  [UserRole.JUNIOR]: PowerLevel.JUNIOR,
+  [UserRole.ASPIRANT]: PowerLevel.ASPIRANT
+};
 
 export class Session {
   @Required()
   @Property()
   userId: string;
+
+  @Required()
+  @Property()
+  username: string;
 
   @Required()
   @Property()
