02ddc5509f8a95d732eea437ad972b668bfbc750
Benjamin Werner <bewr@pm.me>
2024-10-20 22:47:53 +0200

    workaround no production logger

--- src/config/logger/index.ts
+++ src/config/logger/index.ts
@@ -2,35 +2,35 @@ import { $log } from "@tsed/common";
 
 import { isProduction } from "../envs/index";
 
-if (isProduction) {
-  $log.appenders.set("stdout", {
-    type: "stdout",
-    levels: ["info", "debug"],
-    layout: {
-      type: "json"
-    }
-  });
+// if (isProduction) {
+//   $log.appenders.set("stdout", {
+//     type: "stdout",
+//     levels: ["info", "debug"],
+//     layout: {
+//       type: "json"
+//     }
+//   });
 
-  $log.appenders.set("stderr", {
-    levels: ["trace", "fatal", "error", "warn"],
-    type: "stderr",
-    layout: {
-      type: "json"
-    }
-  });
+//   $log.appenders.set("stderr", {
+//     levels: ["trace", "fatal", "error", "warn"],
+//     type: "stderr",
+//     layout: {
+//       type: "json"
+//     }
+//   });
 
-  $log.level = "info";
-} else {
-  $log.appenders.set("stdout", {
-    type: "stdout",
-    levels: ["info", "debug"],
-    layout: {
-      type: "colored"
-    }
-  });
+//   $log.level = "info";
+// } else {
+$log.appenders.set("stdout", {
+  type: "stdout",
+  levels: ["info", "debug"],
+  layout: {
+    type: "colored"
+  }
+});
 
-  $log.level = "debug";
-}
+$log.level = "debug";
+// }
 
 export default {
   disableRoutesSummary: isProduction, // remove table with routes summary
