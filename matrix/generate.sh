ec02e86d6524645f9f57cefef22cef4a61270e96
Benjamin Werner <bewr@pm.me>
2024-10-20 20:35:38 +0200

    fix matrix migrate

--- matrix/generate.sh
+++ matrix/generate.sh
@@ -7,16 +7,9 @@ cp /config/homeserver.yaml.tmpl /data/
 cp /config/chat.faktenforum.log.config /data/
 rm /data/homeserver.yaml
 
-/start.py generate
 # Strip all comments and blank lines from input files
-sed -i -E '/^\s*$|^\s*#/d' /data/homeserver.yaml
+#sed -i -E '/^\s*$|^\s*#/d' /data/homeserver.yaml
 sed -i -E '/^\s*$|^\s*#/d' /data/homeserver.yaml.tmpl
-
-# Delete conflicting keys and arrays from basic config
-yq -i 'del(.listeners, .configbase, .trusted_key_servers)' /data/homeserver.yaml
-
-# Merge and sort the input files alphabetically
-yq -i '. *= load("/data/homeserver.yaml") | sort_keys(..)' /data/homeserver.yaml.tmpl
 
 # Replace all variables using environment variables
 yq -i '(.. | select(tag == "!!str")) |= envsubst' /data/homeserver.yaml.tmpl
