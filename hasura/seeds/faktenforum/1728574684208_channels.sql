29baf2a13d366178e37ca7985b131b3d0b86413f
Benjamin Werner <bewr@pm.me>
2024-10-23 16:16:20 +0200

    Update seeds

--- hasura/seeds/faktenforum/1728574684208_channels.sql
+++ hasura/seeds/faktenforum/1728574684208_channels.sql
@@ -9,11 +9,12 @@ $$ LANGUAGE plpgsql;
 
 DO $$
 BEGIN
-    PERFORM insert_channel_if_not_exists('general', false, false, 'Allgemein', 'General', 'Allgemeine Diskussionen', 'General discussions');
-    PERFORM insert_channel_if_not_exists('introducing_networking', false, false, 'Vorstellungsrunde/Kennenlernen/Vernetzen', 'Introduction/Networking', 'Vorstellungsrunde und Vernetzung', 'Introduction and networking');
-    PERFORM insert_channel_if_not_exists('news_from_correctiv', false, false, 'Neues von Correctiv', 'News from Correctiv', 'Neuigkeiten von Correctiv', 'News from Correctiv');
-    PERFORM insert_channel_if_not_exists('reading_tips', false, false, 'Lesetipps', 'Reading Tips', 'Empfohlene Lektüre', 'Recommended reading');
-    PERFORM insert_channel_if_not_exists('events', false, false, 'Veranstaltungen', 'Events', 'Veranstaltungen', 'Events');
+    PERFORM insert_channel_if_not_exists('general', false, false, 'Allgemein', 'General', 'Diskussionen rund um Desinformation und Faktenchecken – und alles, was euch sonst noch wichtig ist.', 'General discussions');
+    PERFORM insert_channel_if_not_exists('introducing_networking', false, false, 'Kennenlernen & Vernetzen', 'Introduction/Networking', 'Stelle dich vor, lerne andere Mitglieder kennen und vernetze dich.', 'Introduction and networking');
+    PERFORM insert_channel_if_not_exists('news_from_correctiv', false, false, 'Neues von Correctiv', 'News from Correctiv', 'Aktuelle Infos, Updates und Ankündigungen direkt von CORRECTIV.', 'News from Correctiv');
+    PERFORM insert_channel_if_not_exists('reading_tips', false, false, 'Lesetipps', 'Reading Tips', 'Spannende Bücher, Artikel oder Links mit Bezug zu Faktencheck, öffentlicher Debatte und Desinformation.', 'Recommended reading');
+    PERFORM insert_channel_if_not_exists('events', false, false, 'Veranstaltungen', 'Events', 'Workshops, Events und Termine für die Faktenforum-Community.', 'Events');
+    PERFORM insert_channel_if_not_exists('feedback', false, false, 'Feedback & Fragen', 'Feddback', 'Teile dein Feedback zur Plattform und stelle technische Fragen oder Anmerkungen.', 'Events');   
 END $$;
 
 DROP FUNCTION insert_channel_if_not_exists(text, boolean, boolean, text, text, text, text);
