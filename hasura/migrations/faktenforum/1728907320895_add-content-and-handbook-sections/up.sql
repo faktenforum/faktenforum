e0800e64335319ca3ebf3a20af50a9f9bedb7d48
Benjamin Werner <bewr@pm.me>
2024-11-01 20:43:35 +0100

    fix handbook sections table

--- hasura/migrations/faktenforum/1728907320895_add-content-and-handbook-sections/up.sql
+++ hasura/migrations/faktenforum/1728907320895_add-content-and-handbook-sections/up.sql
@@ -7,13 +7,16 @@ CREATE TABLE IF NOT EXISTS page_content (
 
 -- Create the 'handbook_sections' table if it does not exist
 CREATE TABLE IF NOT EXISTS handbook_sections (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     title_de TEXT NOT NULL,
     title_en TEXT NOT NULL,
     teaser_text_de TEXT NOT NULL,
     teaser_text_en TEXT NOT NULL,
-    teaser_image TEXT NOT NULL,
+    teaser_image uuid,
     content_de TEXT NOT NULL,
     content_en TEXT NOT NULL
 );
 
+
+ALTER TABLE ONLY public.handbook_sections
+    ADD CONSTRAINT handbook_sections_teaser_image_fkey FOREIGN KEY (teaser_image) REFERENCES public.file(id) ON UPDATE CASCADE ON DELETE SET NULL;
