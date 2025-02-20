ada425bf21ebd6a28b2d210f4db5063390f5a868
Benjamin Werner <bewr@pm.me>
2025-02-20 10:04:16 +0100

    Fix Drop

--- hasura/migrations/faktenforum/1739970740700_drop_comment_tables/up.sql
+++ hasura/migrations/faktenforum/1739970740700_drop_comment_tables/up.sql
@@ -3,6 +3,6 @@ DELETE FROM "event"
 WHERE table_name IN ('comment', 'comment_user_reactions');
 
 -- Drop the tables
-DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS comment_user_reactions;
 DROP TABLE IF EXISTS comment_history;
+DROP TABLE IF EXISTS comment_user_reactions;
+DROP TABLE IF EXISTS comment;
